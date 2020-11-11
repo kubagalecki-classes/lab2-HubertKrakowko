00289d1150fcfd14a5c51095f5c476f3a6a130cc
HubertKrakowko <01123920@pw.edu.pl>
2020-11-11 19:30:39 +0000

    v0.2

--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -4,45 +4,58 @@
 
 class ResourceManager
 {
-  public:
+ public:
     Resource* R;
-    ResourceManager(): R{} {}
-    ~ResourceManager() {delete R;}
+    ResourceManager() : R{} { cout << "1tworzenie" << endl; }
 
     ResourceManager(const ResourceManager& Rm)
     {
-    Resource* G;
-    G=Rm.R;
-    R=G;
-    delete G;
+        Resource* G;
+        G = Rm.R;
+        R = G;
+        G = nullptr;
+        delete G;
+        //cout << "2kopiuje" << endl;
     }
-    
-    
 
     ResourceManager& operator=(const ResourceManager& Rm)
     {
-        if(&Rm==this)
-        {
-
+        if (&Rm == this) {
         }
-        else{
-          R=nullptr;
-          R=Rm.R;
+        else {
+            R = nullptr;
+            Resource* G;
+            G = Rm.R;
+            R = G;
+           // cout << "3kopiuje=" << endl;
+            G = nullptr;
+            delete G;
         }
         return *this;
     }
 
     ResourceManager(ResourceManager&& Rm)
     {
-        R = Rm.R;
+        R    = Rm.R;
         Rm.R = nullptr;
+        delete Rm.R;
+        //cout << "przeniesienie" << endl;
     }
 
-   ResourceManager& operator=(ResourceManager&& rm) {}
+    ResourceManager& operator=(ResourceManager&& Rm)
+    {
+        R = Rm.R;
+       // cout << "kopia przeniesienia" << endl;
+        Rm.R = nullptr;
+        delete Rm.R;
+        return *this;
+    }
 
-    double get() {return R->get();}
-
-
-
+    double get()
+    {
+        //cout << "k";
+        return R->get();
+    }
+    ~ResourceManager() { delete R; }
 
 };
